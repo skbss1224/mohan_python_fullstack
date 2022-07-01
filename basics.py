2324fdf000595fc647773c93e26ffd1780490032
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-07-01 11:14:40 +0530

    operators
    +-*/%

---
+++ basics.py
@@ -0,0 +1,9 @@
+print("mohan ,mahindra engneering college")
+#operators
+a=90
+b=40
+print("plus operator",a+b)
+print("minus operator",a-b)
+print("div operator",a/b)
+print("mul operator",a*b)
+print("per operator",a%b)
